de3e285ef0f732efeae2423cc96a2b804e94d2e7
Sakaki-Aruka <99568054+Sakaki-Aruka@users.noreply.github.com>
2022-09-09 00:25:22 +0900

    add : reload command

--- src/main/java/menuplugin/menuplugin/MenuPlugin.java
+++ src/main/java/menuplugin/menuplugin/MenuPlugin.java
@@ -1,9 +1,12 @@
 package menuplugin.menuplugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class MenuPlugin extends JavaPlugin {
+public final class MenuPlugin extends JavaPlugin implements CommandExecutor {
 
     public void load(){
         FileConfiguration fileConfiguration = getConfig();
@@ -15,11 +18,26 @@ public final class MenuPlugin extends JavaPlugin {
         saveDefaultConfig();
         this.load();
         getServer().getPluginManager().registerEvents(new Events(),this);
+        getCommand("guimenureload").setExecutor(this);
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command,String label,String[] args){
+        if(sender.isOp()){
+            sender.sendMessage("§cGUI-Menu plugin is updating contents now.");
+            this.load();
+            sender.sendMessage("§bGUI-Menu plugin has finished updated.");
+            return true;
+        }else{
+            sender.sendMessage("§cCannot use.");
+            return false;
+        }
+
     }
 }
